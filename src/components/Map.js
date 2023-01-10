95908921a3a574696ef93d5093f4b80b629d9504
Edward <edward@mycove.com>
2023-01-10 11:11:42 +0700

    Map-Objects-Geo-Shapes: Shapes from arbitrary geographic points

--- src/components/Map.js
+++ src/components/Map.js
@@ -20,6 +20,61 @@ export default class Map extends React.Component {
       lineCap: "square",
       lineJoin: "bevel",
     };
+    // Define points to represent the vertices of a short route in Berlin, Germany:
+    var points = [
+      { lat: 16.5309825, lng: 108.3845921 },
+      { lat: 16.5311923, lng: 108.3853495 },
+      { lat: 16.53108532, lng: 108.3861756 },
+      { lat: 16.5315142, lng: 108.3872163 },
+      { lat: 16.5316215, lng: 108.3885574 },
+      { lat: 16.5320399, lng: 108.3925807 },
+      { lat: 16.5321472, lng: 108.3935785 },
+      { lat: 16.5323832, lng: 108.395499 },
+      { lat: 16.5324261, lng: 108.3959818 },
+      { lat: 16.5325012, lng: 108.397795 },
+      { lat: 16.5325656, lng: 108.3986318 },
+      { lat: 16.5326192, lng: 108.3989215 },
+      { lat: 16.5325119, lng: 108.3989751 },
+      { lat: 16.5323081, lng: 108.3991039 },
+      { lat: 16.5318789, lng: 108.3994472 },
+      { lat: 16.5301194, lng: 108.4009278 },
+      { lat: 16.5297546, lng: 108.4012604 },
+      { lat: 16.5296152, lng: 108.4014106 },
+      { lat: 16.5289822, lng: 108.4018934 },
+      { lat: 16.5276947, lng: 108.4029663 },
+      { lat: 16.5271797, lng: 108.4033203 },
+      { lat: 16.5269973, lng: 108.4033954 },
+      { lat: 16.5265145, lng: 108.4035349 },
+      { lat: 16.5260746, lng: 108.4036851 },
+      { lat: 16.5260103, lng: 108.4038353 },
+      { lat: 16.5256562, lng: 108.40464 },
+      { lat: 16.5253022, lng: 108.4053588 },
+      { lat: 16.5250447, lng: 108.4059381 },
+      { lat: 16.5249588, lng: 108.4062278 },
+      { lat: 16.5249267, lng: 108.4064317 },
+      { lat: 16.5249052, lng: 108.406775 },
+      { lat: 16.5248623, lng: 108.4069574 },
+      { lat: 16.5241864, lng: 108.4089208 },
+      { lat: 16.52410827, lng: 108.4091246 },
+      { lat: 16.5240898, lng: 108.409307 },
+      { lat: 16.524004, lng: 108.4096611 },
+      { lat: 16.5239503, lng: 108.4101653 },
+      { lat: 16.5239289, lng: 108.4110343 },
+      { lat: 16.5238967, lng: 108.4117103 },
+      { lat: 16.5238752, lng: 108.4120321 },
+      { lat: 16.5236285, lng: 108.4126866 },
+      { lat: 16.5231242, lng: 108.41089311 },
+      { lat: 16.5227809, lng: 108.4146714 },
+      { lat: 16.5224799, lng: 108.4152412 },
+    ];
+
+    // Initialize a linestring and add all the points to it:
+    var linestring = new H.geo.LineString();
+    points.forEach(function (point) {
+      linestring.pushPoint(point);
+    });
+    // Initialize a polyline with the linestring:
+    var polyline = new H.map.Polyline(linestring, { style: { lineWidth: 10 } });
 
     // Create a rectangle and pass the custom style as an options parameter:
     var rect = new H.map.Rect(new H.geo.Rect(15.5, 100.5, 17, 108), {
@@ -45,6 +100,7 @@ export default class Map extends React.Component {
       // Add the circle to the map:
       map.addObject(circle);
       map.addObject(rect);
+      map.addObject(polyline);
 
       // attach the listener
       map.addEventListener("mapviewchange", this.handleMapViewChange);
